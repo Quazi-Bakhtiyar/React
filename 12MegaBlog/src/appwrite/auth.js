02303e90d83588a77765a4ea2baa35ed31997ec8
Quazi Bakhtiyar Ali <indianmedievelhistory@gmail.com>
2025-10-11 19:30:17 +0530

    Built authentication service with appwrite

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,66 @@
+import conf from '../conf/conf'
+import {Client, Account ,ID} from 'appwrite';
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setProject(conf.appwriteProjectId)
+            .setEndpoint(conf.appwriteUrl);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email,password,name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(),email,password,name)
+            if(userAccount){
+                // call another method
+                return this.login({email,password})
+            }
+            else{
+                return userAccount;
+                
+            }
+        }
+        catch(err){
+            throw err;
+        }
+    }
+    
+    async login({email,password}){
+        try{
+            return await account.createEmailPasswordSession({email,password})
+        }
+        catch(err){
+            throw err;
+        }
+        return null;
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get()
+        }
+        catch(err){
+            throw err;
+        }
+    }
+
+    async logout(){
+            try{
+                await this.account.deleteSessions()
+            }
+            catch(err){
+                throw err;
+            }
+            
+       
+    }
+}
+
+const authService = new AuthService()
+
+
+export default authService
