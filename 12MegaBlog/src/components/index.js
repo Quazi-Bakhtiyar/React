19768a2e9b752273519a92ea299b31f5c5168c40
Quazi Bakhtiyar Ali <indianmedievelhistory@gmail.com>
2025-10-15 14:59:25 +0530

    implemented Login and Signup complemented using useForm()

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,13 +3,21 @@ import Footer from './Footer/Footer'
 import Container from './Container/Container'
 import Logo from './Logo'
 import LogoutBtn from './Header/LogoutBtn'
+import Input from './Input'
+import PostCard from './PostCard'
+import Select from './Select'
+import Button from './Button'
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Select,
+    Button,
+    PostCard
 }
 
  
